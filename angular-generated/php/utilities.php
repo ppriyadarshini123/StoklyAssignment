d7bee226716a6a81d7450c461433effb7699d510
payal <payal.priyadarshini@gmail.com>
2021-03-08 14:39:34 +0000

    added item service file

---
+++ angular-generated/php/utilities.php
@@ -0,0 +1,7 @@
+<?php
+
+//===========  CONSTANTS:
+###### defining a constant pointing at the root folder
+//define('CONST_NAME','VALUE');
+define('ROOT', 'http://localhost/StoklyAssignment/StoklyAssignment/angular-generated/');
+?>
---
+++ angular-generated/src/app/item.ts
@@ -0,0 +1,5 @@
+export class Item {
+    
+  constructor(  item: string, ID?: number ) {}
+
+  }
